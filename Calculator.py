48f68bbd4362685ba2203e9762e3d8b07262b15e
Gmart0 <novayazemlya2015@gmail.com>
2021-04-12 22:18:16 +0300

    parse implementation and null import test

--- Calculator.py
+++ Calculator.py
@@ -5,12 +5,44 @@ class Calculator:
         self.second_operand = 0
         self.operator = ""
         self.newline = ""
+        self.answer = ""
 
     def read_file(self, program_input):
         self.parse(program_input)
 
-    def parse(self, program_input):
-        self.newline = program_input
-        # return newline
+    def remove_spaces(self, program_input):
+        return program_input.replace(" ", "")
 
+    def parse(self, program_input):
+        self.newline = self.remove_spaces(program_input)
+        temp_line_1 = ""
+        temp_line_2 = ""
+        temp_operator = ""
+        temp_operator2 = ""
+        counter_splitter = 0
+        if len(self.newline) != 0:
+            for char in range(len(self.newline)):
+                temp_line_1 += self.newline[char]
+                counter_splitter += 1
+                if not self.newline[char].isnumeric():
+                    temp_operator += self.newline[char]
+                    break
+            if len(temp_operator) != 0:
+                self.first_operand = temp_line_1[:-1]
+            else:
+                self.first_operand = temp_line_1
+            # print("1 op =", self.first_operand)
+            if len(self.newline) - counter_splitter != 0:
+                for char in range(counter_splitter, len(self.newline)):
+                    temp_line_2 += self.newline[char]
+                    if not self.newline[char].isnumeric():
+                        temp_operator2 += self.newline[char]
+                        break
+                if len(temp_operator2) != 0:
+                    self.second_operand = temp_line_2[:-1]
+                else:
+                    self.second_operand = temp_line_2
+                # print("2 op =", self.second_operand)
+        else:
+            self.first_operand = "0"
 
--- test_calculator.py
+++ test_calculator.py
@@ -15,4 +15,15 @@ class InputFileTest(TestCalculator):
             program_input = file.read()
 
         self.calculator.read_file(program_input)
-        self.assertEqual(self.calculator.newline, "5 7 - 1")
+        self.assertEqual(self.calculator.newline, "4657-1263", "information form input file was not read correctly")
+
+
+class NullInputTest(TestCalculator):
+
+    def test_read_file(self):
+        with open("nullfile.txt", "r") as file:
+            program_input = file.read()
+
+        self.calculator.read_file(program_input)
+        self.assertEqual(self.calculator.first_operand, "0", "Null input gives incorrect result")
+
