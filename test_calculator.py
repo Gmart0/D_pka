a39c641cd3566593b6341e8bc23fe3874a97e83e
Gmart0 <novayazemlya2015@gmail.com>
2021-04-12 22:21:59 +0300

    only one number input test

--- test_calculator.py
+++ test_calculator.py
@@ -27,3 +27,13 @@ class NullInputTest(TestCalculator):
         self.calculator.read_file(program_input)
         self.assertEqual(self.calculator.first_operand, "0", "Null input gives incorrect result")
 
+
+class OneNumberTest(TestCalculator):
+
+    def test_read_file(self):
+        with open("only_one_num.txt", "r") as file:
+            program_input = file.read()
+
+        self.calculator.read_file(program_input)
+        self.assertEqual(self.calculator.first_operand, "42", "incorrect result with only one number")
+
